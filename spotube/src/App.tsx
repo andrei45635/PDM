810a0781c273e85a251ef79ee3e9befb4e4b0169
andrei45635 <iacob.andrei34@gmail.com>
2023-10-25 23:07:02 +0300

    lab1 ionic commit 2

---
+++ spotube/src/App.tsx
@@ -0,0 +1,91 @@
+// import { Redirect, Route } from 'react-router-dom';
+// import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+// import { IonReactRouter } from '@ionic/react-router';
+// import Home from './pages/Home';
+// import ViewMessage from './pages/ViewMessage';
+//
+// /* Core CSS required for Ionic components to work properly */
+// import '@ionic/react/css/core.css';
+//
+// /* Basic CSS for apps built with Ionic */
+// import '@ionic/react/css/normalize.css';
+// import '@ionic/react/css/structure.css';
+// import '@ionic/react/css/typography.css';
+//
+// /* Optional CSS utils that can be commented out */
+// import '@ionic/react/css/padding.css';
+// import '@ionic/react/css/float-elements.css';
+// import '@ionic/react/css/text-alignment.css';
+// import '@ionic/react/css/text-transformation.css';
+// import '@ionic/react/css/flex-utils.css';
+// import '@ionic/react/css/display.css';
+//
+// /* Theme variables */
+// import './theme/variables.css';
+//
+// setupIonicReact();
+//
+// const App: React.FC = () => (
+//   <IonApp>
+//     <IonReactRouter>
+//       <IonRouterOutlet>
+//         <Route path="/" exact={true}>
+//           <Redirect to="/home" />
+//         </Route>
+//         <Route path="/home" exact={true}>
+//           <Home />
+//         </Route>
+//         <Route path="/message/:id">
+//            <ViewMessage />
+//         </Route>
+//       </IonRouterOutlet>
+//     </IonReactRouter>
+//   </IonApp>
+// );
+//
+// export default App;
+
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+
+/* Core CSS required for Ionic components to work properly */
+import '@ionic/react/css/core.css';
+
+/* Basic CSS for apps built with Ionic */
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
+
+/* Optional CSS utils that can be commented out */
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
+
+/* Theme variables */
+import './theme/variables.css';
+
+import {SongEdit, SongList} from "./todo";
+import {SongProvider} from "./todo/SongProvider";
+
+setupIonicReact();
+
+const App: React.FC = () => (
+    <IonApp>
+        <SongProvider>
+            <IonReactRouter>
+                <IonRouterOutlet>
+                    <Route path="/songs" component={SongList} exact={true}/>
+                    <Route path="/song" component={SongEdit} exact={true}/>
+                    <Route path="/song/:id" component={SongEdit} exact={true}/>
+                    <Route exact path="/" render={() => <Redirect to="/songs"/>}/>
+                </IonRouterOutlet>
+            </IonReactRouter>
+        </SongProvider>
+    </IonApp>
+);
+
+export default App;
